e0ec847f75e6b1b312395d637f0bd837e06291a6
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2024-03-07 18:19:14 +0530

    Fixes configure script
    Signed-off-by: Saahil Bhavsar <saahil_bhavsar@outlook.com>

--- scripts/configure-opk-server.sh
+++ scripts/configure-opk-server.sh
@@ -10,29 +10,29 @@ fi
 
 if sudo -nl &> /dev/null || $(id -u) -ne 0; then
     echo "User has sudo privileges without a password."
-    # Create the default opk directory
+    # Create the default sos directory
     sudo mkdir -p /etc/sos
 
-    # Install the opk verifier
-    sudo mv opk-ssh /etc/sos
-    sudo chown root /etc/sos/opk-ssh
-    sudo chmod 700 /etc/sos/opk-ssh
+    # Install the sos verifier
+    sudo mv verifier /etc/sos
+    sudo chown root /etc/sos/verifier
+    sudo chmod 700 /etc/sos/verifier
 
     # Create root policy yaml file which is a yaml file
     sudo touch /etc/sos/policy.yml
     sudo chown root /etc/sos/policy.yml
     sudo chmod 600 /etc/sos/policy.yml
 
-    sudo /etc/opk/opk-ssh add "${EMAIL}" "${USER}"
+    sudo /etc/sos/verifier add "${EMAIL}" "${USER}"
 
     # Comment out existing AuthorizedKeysCommand configuration
     # TODO: How do these other AuthorizedKeysCommands interact with our own?
     sudo sed -i '/^AuthorizedKeysCommand /s/^/#/' "/etc/ssh/sshd_config"
     sudo sed -i '/^AuthorizedKeysCommandUser /s/^/#/' "/etc/ssh/sshd_config"
 
-    # Add our AuthorizedKeysCommand line so that the opk verifier is called when ssh-ing in
+    # Add our AuthorizedKeysCommand line so that the sos verifier is called when ssh-ing in
     sudo tee -a /etc/ssh/sshd_config > /dev/null <<EOT
-AuthorizedKeysCommand /etc/opk/opk-ssh verify %u %k %t
+AuthorizedKeysCommand /etc/sos/verifier verify %u %k %t
 AuthorizedKeysCommandUser root
 EOT
 
@@ -43,14 +43,14 @@ else
 
     mkdir ~/.sos
 
-    # Install the opk-ssh client to user's home directory
-    mv opk-ssh ~/.sos
-    chmod 700 ~/.sos/opk-ssh
+    # Install the verifier client to user's home directory
+    mv verifier ~/.sos
+    chmod 700 ~/.sos/verifier
 
     # Create a personal policy yaml file in user's home directory
     touch ~/.sos/policy.yml
     chmod 600 ~/.sos/policy.yml
 
-    ~/.opk/opk-ssh add "${EMAIL}" "${USER}"
+    ~/.sos/verifier add "${EMAIL}" "${USER}"
     exit 0
 fi
