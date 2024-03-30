391ff7ba6c8a2599bb6500966d5c7d209c270e41
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2024-03-31 01:10:54 +0530

    Adds constants
    Signed-off-by: Saahil Bhavsar <saahil_bhavsar@outlook.com>

---
+++ internal/pkg/constants/constants.go
@@ -0,0 +1,28 @@
+package constants
+
+import (
+	"fmt"
+
+	"github.com/devlup-labs/sos/openpubkey/client/providers"
+)
+
+var (
+	clientID = "992028499768-ce9juclb3vvckh23r83fjkmvf1lvjq18.apps.googleusercontent.com"
+	// The clientSecret was intentionally checked in. It holds no power and is used for development. Do not report as a security issue
+	clientSecret = "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" // Google requires a ClientSecret even if this a public OIDC App
+	scopes       = []string{"openid profile email"}
+	redirURIPort = "3000"
+	callbackPath = "/login-callback"
+	redirectURI  = fmt.Sprintf("http://localhost:%v%v", redirURIPort, callbackPath)
+)
+
+var (
+	Op = providers.GoogleOp{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Scopes:       scopes,
+		RedirURIPort: redirURIPort,
+		CallbackPath: callbackPath,
+		RedirectURI:  redirectURI,
+	}
+)
